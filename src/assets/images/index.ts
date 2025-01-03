2d25d8be49fe6e4d36d50cbf4b6cf547ecf06dc1
Jay Khatri <jay.khatri@oorjan.com>
2025-01-03 13:28:58 +0530

    feat(react app configured): typescript, tailwind css, images, structure

---
+++ src/assets/images/index.ts
@@ -0,0 +1,19 @@
+import avatarAngelaGray from "./avatar-angela-gray.webp";
+import avatarAnnaKim from "./avatar-anna-kim.webp";
+import avatarJacobThompson from "./avatar-jacob-thompson.webp";
+import avatarKimberlySmith from "./avatar-kimberly-smith.webp";
+import avatarMarkWebber from "./avatar-mark-webber.webp";
+import avatarNathanPeterson from "./avatar-nathan-peterson.webp";
+import avatarRizkyHasanuddin from "./avatar-rizky-hasanuddin.webp";
+import imageChess from "./image-chess.webp";
+
+export {
+  avatarAngelaGray,
+  avatarAnnaKim,
+  avatarJacobThompson,
+  avatarKimberlySmith,
+  avatarMarkWebber,
+  avatarNathanPeterson,
+  avatarRizkyHasanuddin,
+  imageChess,
+};
